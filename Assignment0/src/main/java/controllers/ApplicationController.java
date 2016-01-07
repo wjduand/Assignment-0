c3b892fce59c7d63e9f8c0668e42ff22f1726569
HouPoc <hou199488@gmail.com>
2016-01-06 19:06:41 -0800

    test

--- Assignment0/src/main/java/controllers/ApplicationController.java
+++ Assignment0/src/main/java/controllers/ApplicationController.java
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * I tried to add inforamtion but failed
  */
 
 package controllers;
