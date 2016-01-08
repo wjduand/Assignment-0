7020cc1926906dfdb608be64f9e952c5a2a35eec
HouPoc <hou199488@gmail.com>
2016-01-07 17:39:19 -0800

    Assignment 0
    Add personal information and test

--- Assignment0/src/test/java/Model/RanSuccessfullyListTest.java
+++ Assignment0/src/test/java/Model/RanSuccessfullyListTest.java
@@ -22,6 +22,8 @@ public class RanSuccessfullyListTest {
         rsl.addPerson(p);
         p = new Person("Taylor Kirkpatrick","kirkpatt","Windows");
         rsl.addPerson(p);
-        assertEquals(2,rsl.getPersons().size());
+        p = new Person("Wenbo Hou","HouPoc","Windows");
+        rsl.addPerson(p);
+        assertEquals(3,rsl.getPersons().size());
     }
 }
