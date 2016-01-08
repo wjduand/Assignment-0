f532a90f94e5334811354b367b63d86b37cad745
zhengxiaoyong <zhenxiao@oregonstate.edu>
2016-01-08 14:49:06 -0800

    Assignment0
    I add my name

--- Assignment0/src/main/java/controllers/ApplicationController.java
+++ Assignment0/src/main/java/controllers/ApplicationController.java
@@ -41,6 +41,7 @@ public class ApplicationController {
         rsf.addPerson(new Person("Taylor Kirkpatrick","kirkpatt","Windows"));
         rsf.addPerson(new Person("Wenbo Hou", "HouPoc", "Windows"));
         rsf.addPerson(new Person("Vasile Grejuc", "grejucv", "Mac"));
+        rsf.addPerson(new Person("Xiaoyong Zheng","zhengxiaoyong","Windows"));
 
         return Results.json().render(rsf);
 
--- Assignment0/src/test/java/controllers/ApiControllerDocTesterTest.java
+++ Assignment0/src/test/java/controllers/ApiControllerDocTesterTest.java
@@ -57,7 +57,7 @@ public class ApiControllerDocTesterTest extends NinjaDocTester {
 
         RanSuccessfullyList ranSuccesfully = response.payloadJsonAs(RanSuccessfullyList.class);
 
-        assertEquals(4,ranSuccesfully.getPersons().size());
+        assertEquals(5,ranSuccesfully.getPersons().size());
 
     
     }
